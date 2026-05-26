106d41d8b73215f41004cfac60d55a281c5907b5
MOOD PRAVEENKUMAR NAIK <mpraveenkumarnaik021@gmail.com>
2026-05-26 21:10:22 +0530

    initial commit

---
+++ FirstNewProject/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,31 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+					
+		features= {".//Features/Registration.feature"},
+		//features= {"@target/rerun.txt"},
+		glue={"stepDefinition","hooks"},
+		plugin= {
+					"pretty", "html:reports/myreport.html",   
+					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+					"rerun:target/rerun.txt",
+				
+				},
+				
+		dryRun=false,    // checks mapping between scenario steps and step definition methods
+		monochrome=true,    // to avoid junk characters in output
+		publish=true   // to publish report in cucumber server
+		
+					
+		)
+public class TestRunner {
+
+}
+
+
